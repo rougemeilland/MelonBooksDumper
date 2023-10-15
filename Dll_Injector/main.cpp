46db8cecdc81af9af3d3c488bec79c6e47cedb09
rougemeilland <28302784+rougemeilland@users.noreply.github.com>
2023-10-16 02:17:26 +0900

    Re-marge fix issue #4

--- Dll_Injector/main.cpp
+++ Dll_Injector/main.cpp
@@ -2,9 +2,12 @@
 #include"injector.h"
 #include <stdlib.h>
 #include <iostream>
+#include "interprocess.h"
 
 #define PROCESS_NAME "melonbooksviewer.exe"
 #define DLL_NAME "MelonDumper.dll"
+
+HANDLE InjectionCompletionEventHandle;
 
 // usage: Dll_Injector.exe [--path <initial path to save image file>]
 int main(int argc, const char *argv[])
@@ -18,12 +21,15 @@ int main(int argc, const char *argv[])
 			initialPath = argv[index];
 		}
 	}
+	InjectionCompletionEventHandle = ::CreateEvent(NULL, TRUE, FALSE, INJECTION_COMPLETION_EVENT_NAME);
+	if (InjectionCompletionEventHandle == NULL)
+		return EXIT_FAILURE;
 	std::string path = BrowseFolder(initialPath);
 	if (path == "")return EXIT_SUCCESS;
 	set_global_path(path);
 
-	InjectDLL(getProcID(PROCESS_NAME), GetBasePath()+"\\"+DLL_NAME);
-	Sleep(1000);
+	InjectDLL(getProcID(PROCESS_NAME), GetBasePath()+"\\"+DLL_NAME, InjectionCompletionEventHandle);
+
 	std::cout << "Exiting..." << endl;
 	return EXIT_SUCCESS;
 }
